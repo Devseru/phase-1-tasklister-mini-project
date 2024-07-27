504cab0ddbcf40875b1f4fd6367eea1dc8825adf
Devseru <serushikoli65@gmail.com>
2024-07-27 20:03:42 +0300

    completing tasklister

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault(); 
+  
+      const newTaskDescription = document.getElementById("new-task-description").value;
+  
+      const newTask = document.createElement("li");
+      newTask.textContent = newTaskDescription;
+  
+      taskList.appendChild(newTask);
+  
+      event.target.reset();
+    });
+  });
+  
